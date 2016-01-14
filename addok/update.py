0ddaa2cddb47823044da3663afb3fa1f7d3c7640
Apside-bdx-BBO <bonheur.0692@apside-groupe.com>
2016-01-14 17:56:10 +0100

    import from ban api ok

--- addok/update.py
+++ addok/update.py
@@ -1,14 +1,11 @@
-import json
 import os
-from pathlib import Path
-from collections import deque
-from pip._vendor.distlib.compat import raw_input
 
 import requests
+from urllib.parse import urlparse
 
 from addok.batch.utils import process
 
-BAN_SERVER = "http://ban-dev.data.gouv.fr:80"
+BAN_SERVER = "http://localhost:5959"  # "http://ban-dev.data.gouv.fr:80"
 PATH = 'C:\\Users\\mhx157'
 
 
@@ -20,19 +17,41 @@ def get_response():
 
     # resp = make_a_way(None, 'street', 1)
     # print(resp)
-    # process(resp)
-    with Path(os.path.join(PATH, 'allBAN_addok.json')).open(mode='w') as f:
-        for insee in range(33000, 34000):
-            _insee = 'insee:' + str(insee)
-            # extract_ban_process(_insee)
+    diffs = get_diff()
+    for diff in diffs['collection']:
+        if diff['resource'] == 'municipality':
+            print('resource: municipality')
+        elif diff['resource'] == 'housenumber':
+            if diff['new'].get('locality'):
+                process(make_a_way('update', 'locality', diff['new']['locality']))
+            elif diff['new'].get('street'):
+                process(make_a_way('update', 'locality', diff['new']['street']))
+            elif diff['new'].get('districts'):
+                process(make_a_way('update', 'locality', diff['new']['districts']))
+        else:
+            process(make_a_way('update', diff.get('resource'), diff.get('resource_id')))
+        print('process ok')
 
-            extract_ban_to_file(_insee, f)
+        # with Path(os.path.join(PATH, 'allBAN_addok.json')).open(mode='w') as f:
+        #     for insee in range(33000, 34000):
+        #         _insee = 'insee:' + str(insee)
+        #         # extract_ban_process(_insee)
+        #
+        #         extract_ban_to_file(_insee, f)
+
+        # url = "http://ban-dev.data.gouv.fr/municipality/15076/localities"
+        # resp = make_ways(None, request_call(url))
+        # print(resp)
 
 
+def get_housenumber_way(housenumber_id):
+    uri = BAN_SERVER + "/housenumber/{}".format(housenumber_id)
+    return request_call(uri)
 
-            # url = "http://ban-dev.data.gouv.fr/municipality/15076/localities"
-            # resp = make_ways(None, request_call(url))
-            # print(resp)
+
+def get_diff(increment=''):
+    uri = BAN_SERVER + "/diff/?increment={}".format(increment)
+    return request_call(uri)
 
 
 def extract_ban_to_file(municipality_id, f):
@@ -89,7 +108,7 @@ def get_ways_by_municipality(ways):
         for way in ways['collection']:
             response.append(way)
         try:
-            ways = request_call(ways['next'])
+            ways = request_call(BAN_SERVER + urlparse(ways.get('next')).get('path'))
         except:
             break
     return response
@@ -116,22 +135,14 @@ def make_municipality(action, municipality_id):
 def make_a_way(action, resource_name, resource_id):
     uri = BAN_SERVER + "/{}/{}".format(resource_name, resource_id)
     way = request_call(uri)
-    housenumbers = request_call(uri + '/housenumbers/')
-    importance = 1
-    hns = {}
-    orig_center = {'lon': 0, 'lat': 0, 'postcode': None}
 
-    while True:
-        for hn in housenumbers['collection']:
-            hns.update(make_a_housenumber(hn, orig_center))
-        try:
-            housenumbers = request_call(housenumbers['next'])
-        except:
-            break
+    importance = 1
+    hns = get_housenumbers(uri)
+    default_orig_center = {'lon': 0, 'lat': 0, 'postcode': None}
 
     json_resp = {'id': way.get('municipality').get('insee') + '_' + way.get('fantoir'), 'type': resource_name,
                  'name': way.get('name'), 'insee': way.get('municipality').get('insee'),
-                 'postcode': orig_center.get('postcode'), 'lon': orig_center.get('lon'), 'lat': orig_center.get('lat'),
+                 'postcode': way.get('postcode', ''), 'lon': way.get('lon', 0), 'lat': way.get('lat', 0),
                  'city': way.get('municipality').get('name'),
                  'context': way.get('name') + ',' + way.get('municipality').get('name'),
                  'importance': get_importance(way.get('name')),
@@ -142,17 +153,32 @@ def make_a_way(action, resource_name, resource_id):
     return json_resp
 
 
-def make_a_housenumber(hn, orig_center):
+def get_housenumbers(uri):
+    hns = {}
+    housenumbers = request_call(uri + '/housenumbers/')
+    while True:
+        for hn in housenumbers['collection']:
+            hns.update(make_a_housenumber(hn))
+        try:
+            housenumbers = request_call(BAN_SERVER + urlparse(housenumbers.get('next')).get('path'))
+        except:
+            break
+    return hns
+
+
+def make_a_housenumber(hn):
     if hn['number'] != 0:
         return {(hn['number'] + ' ' + hn['ordinal']).strip(): {'lat': hn['center']['coordinates'][0],
                                                                'lon': hn['center']['coordinates'][1], 'id': hn['cia'],
                                                                'cea': hn['laposte'],
-                                                               'postcode': orig_center['postcode']}}
+                                                               'postcode': hn['postcode']}}
 
 
 def request_call(url):
     try:
-        response = requests.get(url)
+        os.environ['NO_PROXY'] = 'localhost'
+        headers = {'Authorization': 'Bearer token'}
+        response = requests.get(url, headers=headers)
     except requests.exceptions.Timeout:
         # Maybe set up for a retry, or continue in a retry loop
         return None
